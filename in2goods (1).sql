f737eafffc939620e57d7d77f6e9244504842cc4
Albin P Thomas <75466940+albinpthomas@users.noreply.github.com>
2022-05-03 08:25:53 +0530

    Add files via upload

--- in2goods (1).sql
+++ in2goods (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 07, 2022 at 03:52 AM
+-- Generation Time: May 03, 2022 at 02:55 AM
 -- Server version: 5.7.31
 -- PHP Version: 7.3.21
 
@@ -34,20 +34,22 @@ CREATE TABLE IF NOT EXISTS `car_category` (
   `car_type` varchar(10) NOT NULL,
   `company_id` int(10) NOT NULL,
   PRIMARY KEY (`car_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `car_category`
 --
 
 INSERT INTO `car_category` (`car_id`, `car_details`, `car_type`, `company_id`) VALUES
-(9, 'i20', 'Hatchback', 3),
+(9, 'i20', 'Hatchback', 5),
 (15, 'wagoner', 'sedan', 1),
 (30, 'innova', 'suv', 2),
-(31, 'Etios liva', 'Hackback', 2),
+(31, 'Etios liva', 'Hatchback', 2),
 (33, 'city', 'crossover', 4),
-(34, 'Alto', 'Hackback', 1),
-(35, 'Harrier', 'suv', 3);
+(34, 'Alto', 'Hatchback', 1),
+(35, 'Harrier', 'suv', 3),
+(36, 'innova', 'suv', 1),
+(37, 'i20', 'hyundai', 1);
 
 -- --------------------------------------------------------
 
@@ -72,32 +74,7 @@ INSERT INTO `car_company` (`company_id`, `company_name`) VALUES
 (3, 'Tata  '),
 (4, 'Honda '),
 (5, 'Hyundai'),
-(6, 'Others');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `company`
---
-
-DROP TABLE IF EXISTS `company`;
-CREATE TABLE IF NOT EXISTS `company` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `company` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `company`
---
-
-INSERT INTO `company` (`id`, `company`) VALUES
-(1, 'Maruthi Suzuki'),
-(2, 'Toyota'),
-(3, 'Tata'),
-(4, 'Honda'),
-(5, 'Hyundai'),
-(6, 'Others');
+(6, 'Mini');
 
 -- --------------------------------------------------------
 
@@ -113,29 +90,34 @@ CREATE TABLE IF NOT EXISTS `tbl_items` (
   `items_name` varchar(110) NOT NULL,
   `company_id` int(11) NOT NULL,
   `ad_type` varchar(200) NOT NULL,
-  `negotible` int(2) NOT NULL DEFAULT '1',
+  `negotible` varchar(5) NOT NULL,
   `contact_name` varchar(20) NOT NULL,
   `contact_number` varchar(20) NOT NULL,
-  `contact_email` varchar(20) NOT NULL,
+  `contact_email` varchar(200) NOT NULL,
   `contact_address` varchar(50) NOT NULL,
   `items_img` varchar(500) NOT NULL,
   `items_category` varchar(110) NOT NULL,
   `items_price` int(11) NOT NULL,
+  `km` varchar(100) NOT NULL,
+  `insurance_type` varchar(100) NOT NULL,
+  `Registration` varchar(100) NOT NULL,
+  `Fuel_type` varchar(100) NOT NULL,
+  `Ownership` int(20) NOT NULL,
   `items_desc` varchar(500) NOT NULL,
   `items_status` int(11) NOT NULL DEFAULT '1',
   PRIMARY KEY (`items_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_items`
 --
 
-INSERT INTO `tbl_items` (`items_id`, `user_id`, `ad_title`, `items_name`, `company_id`, `ad_type`, `negotible`, `contact_name`, `contact_number`, `contact_email`, `contact_address`, `items_img`, `items_category`, `items_price`, `items_desc`, `items_status`) VALUES
-(8, 129, '2011 innova for sale', 'innova', 0, '0', 1, 'Albin P Thomas', '9846173680', 'albin@gmail.com', 'padinjareparambil.', 'assets/images/1638999394.png', 'suv', 100000, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,', 1),
-(9, 129, 'Hector for sale', 'Hector', 0, '0', 1, 'Albin P Thomas', '9846173680', 'abcd@gmail.com', 'padinjareparambil.', 'assets/images/1638999556.jpg', 'suv', 500000, 'good condition vehicle ', 1),
-(11, 129, 'i20', 'i20', 0, '0', 1, 'Albin P Thomas', '9846173680', 'abcd@gmail.com', 'padinjareparambil.', 'assets/images/1639021787.jpg', 'Hatchback', 1200000, 'good vehiclee\r\nshowroom serviced\r\nwell maintained\r\n', 1),
-(14, 129, 'good condition car for sale', 'mini', 6, 'personal', 1, 'Albin P Thomas', '9846173680', 'albin@gmail.com', 'padinjareparambil.', 'assets/images/1639718607.jpeg', 'cooper', 100000, 'good vehiclee\r\nshowroom serviced\r\nwell maintained\r\n', 1),
-(15, 132, 'good vehicle for sale', 'manaza', 3, 'personal', 1, 'soorya', '9846173680', 'soorya@gmail.com', 'soorya bhavan', 'assets/images/1639729330.jpeg', 'suv', 500000, 'good vehicle\r\ncompany service\r\ngood tyres\r\nshowroom condition', 1);
+INSERT INTO `tbl_items` (`items_id`, `user_id`, `ad_title`, `items_name`, `company_id`, `ad_type`, `negotible`, `contact_name`, `contact_number`, `contact_email`, `contact_address`, `items_img`, `items_category`, `items_price`, `km`, `insurance_type`, `Registration`, `Fuel_type`, `Ownership`, `items_desc`, `items_status`) VALUES
+(8, 129, '2011 innova for sale', 'innova', 2, '0', '1', 'Albin P Thomas', '9846173680', 'albin@gmail.com', 'padinjareparambil.', 'assets/images/1638999394.png', 'suv', 100000, '', '', '', '', 0, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries,', 1),
+(9, 129, 'Hector for sale', 'Hector', 3, '0', '1', 'Albin P Thomas', '9846173680', 'abcd@gmail.com', 'padinjareparambil.', 'assets/images/1638999556.jpg', 'suv', 500000, '', '', '', '', 0, 'good condition vehicle ', 1),
+(11, 129, 'i20', 'i20', 5, '0', '1', 'Albin P Thomas', '9846173680', 'abcd@gmail.com', 'padinjareparambil.', 'assets/images/1639021787.jpg', 'Hatchback', 1200000, '', '', '', '', 0, 'good vehiclee\r\nshowroom serviced\r\nwell maintained\r\n', 1),
+(14, 129, 'good condition car for sale', 'mini', 6, 'personal', '1', 'Albin P Thomas', '9846173680', 'albin@gmail.com', 'padinjareparambil.', 'assets/images/1639718607.jpeg', 'cooper', 100000, '', '', '', '', 0, 'good vehiclee\r\nshowroom serviced\r\nwell maintained\r\n', 1),
+(15, 132, 'good vehicle for sale', 'manaza', 3, 'personal', '1', 'soorya', '9846173680', 'soorya@gmail.com', 'soorya bhavan', 'assets/images/1639729330.jpeg', 'suv', 500000, '', '', '', '', 0, 'good vehicle\r\ncompany service\r\ngood tyres\r\nshowroom condition', 1);
 
 -- --------------------------------------------------------
 
@@ -149,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `tbl_save` (
   `user_id` int(20) NOT NULL,
   `items_id` int(20) NOT NULL,
   PRIMARY KEY (`save_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_save`
@@ -157,8 +139,8 @@ CREATE TABLE IF NOT EXISTS `tbl_save` (
 
 INSERT INTO `tbl_save` (`save_id`, `user_id`, `items_id`) VALUES
 (19, 131, 14),
-(28, 129, 9),
 (15, 129, 12),
+(34, 191, 9),
 (22, 134, 9),
 (23, 134, 8),
 (24, 134, 14);
@@ -178,7 +160,7 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
   `name` varchar(10) NOT NULL,
   `user_created_at` varchar(10) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=193 DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_user`
@@ -186,9 +168,8 @@ CREATE TABLE IF NOT EXISTS `tbl_user` (
 
 INSERT INTO `tbl_user` (`user_id`, `username`, `password`, `user_status`, `name`, `user_created_at`) VALUES
 (116, 'abcdef@gmail.com', 'e807f1fcf82d132f9bb018ca6738a19f', 0, 'abbcdds aj', '11-11-21'),
-(118, 'admin@gmail.com', '7ece99e593ff5dd200e2b9233d9ba654', 2, 'admin admi', '11-11-21'),
 (120, 'rubinsiby@mca.ajce.in', 'a35e723cc29d1ec9a23566cfffe77b12', 0, 'rubin siby', '12-11-21'),
-(121, 'frankmathewsthomas@gmail.com', '25d55ad283aa400af464c76d713c07ad', 1, 'frank thom', '12-11-21'),
+(121, 'frankmathewsthomas@gmail.com', '25d55ad283aa400af464c76d713c07ad', 0, 'frank thom', '12-11-21'),
 (122, 'ligin@gmail.com', '383b7d44a65cf9f0e8a474427f01eeba', 1, 'ligin abra', '22-11-21'),
 (123, 'frank@gmail.com', '25d55ad283aa400af464c76d713c07ad', 1, 'frank thom', '22-11-21'),
 (126, 'vivek@gmail.com', '25f9e794323b453885f5181f1b624d0b', 1, 'Vivek Kuma', '24-11-21'),
@@ -199,7 +180,9 @@ INSERT INTO `tbl_user` (`user_id`, `username`, `password`, `user_status`, `name`
 (131, 'ak@gmail.com', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 'Akhil Siby', '15-12-21'),
 (132, 'soorya@gmail.com', 'd91a96c788cddf037b5934efff6c3fc3', 1, 'soorya p', '17-12-21'),
 (133, 'kevin@gmail.com', 'a588f61c13a948386072c24916554b58', 1, 'kevin liza', '17-12-21'),
-(134, 'abc@gmail.com', '97ac82a5b825239e782d0339e2d7b910', 1, 'abc abc', '17-12-21');
+(134, 'abc@gmail.com', '97ac82a5b825239e782d0339e2d7b910', 1, 'abc abc', '17-12-21'),
+(191, 'admin@gmail.com', 'e495a909893e9f1d90198016848fc344', 2, 'admin t', '05-04-22'),
+(192, 'mrudul@gmail.in', 'e8be9ef4840be0bd6b1ce18ac2900108', 0, 'Mrudul A', '06-04-22');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
